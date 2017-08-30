d1595bd45359c313f2553d49b67e4c91b510d431
dumbo0001 <dumbo0001@gmail.com>
2017-08-31 00:01:31 +0200

    Add first steps of FT for layout and styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -82,10 +82,32 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-        
+
         # Satisfied, they both go back to sleep
 
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-        
+
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
