ba8dea9ca7a898701e81c255ed153ccfc3fa71c1
dumbo0001 <dumbo0001@gmail.com>
2018-01-30 20:29:14 +0100

    Use document ready in script

--- superlists/lists/static/list.js
+++ superlists/lists/static/list.js
@@ -1,3 +1,5 @@
-$('input').on('keypress', function () { //1
-    $('.has-error').hide();
+jQuery(document).ready(function ($) {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
